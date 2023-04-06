3912cc8c8376ea6a5ff570052b178fb237d94491
diroy41 <diroy41@hotmail.com>
2023-04-06 18:27:37 -0500

    Started on the CoreJavaBasics Operators and Numbers

---
+++ src/com/perscholas/java_operatorsNumbers/exercise1.java
@@ -0,0 +1,10 @@
+package com.perscholas.java_operatorsNumbers;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        int x = 2;
+
+        System.out.println(Integer.toBinaryString(x));
+
+    }
+}
