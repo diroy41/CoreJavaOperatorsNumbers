b3b6d3a02c2b73a1e00d50ae833618cd0264268a
diroy41 <diroy41@hotmail.com>
2023-04-09 12:49:31 -0500

    Completed Ex1 and Ex2

--- src/com/perscholas/java_operatorsNumbers/exercise1.java
+++ src/com/perscholas/java_operatorsNumbers/exercise1.java
@@ -3,8 +3,32 @@ package com.perscholas.java_operatorsNumbers;
 public class exercise1 {
     public static void main(String[] args) {
         int x = 2;
-
         System.out.println(Integer.toBinaryString(x));
+        //I think the decimal will be around 3-5 and the binary will be 100
+        x = x << 1;
+        System.out.println(x);
+        System.out.println(Integer.toBinaryString(x) + "\n");
+
+        int x2 = 9;
+        System.out.println(Integer.toBinaryString(x2));
+        x2 = x2 << 1;
+        System.out.println(x2);
+        System.out.println(Integer.toBinaryString(x2)+ "\n");
+
+        int x3 = 17;
+        System.out.println(Integer.toBinaryString(x3));
+        x3 = x3 << 1;
+        System.out.println(x3);
+        System.out.println(Integer.toBinaryString(x3)+ "\n");
+
+        int x4 = 88;
+        System.out.println(Integer.toBinaryString(x4));
+        x4 = x4 << 1;
+        System.out.println(x4);
+        System.out.println(Integer.toBinaryString(x4)+ "\n");
+
+
+
 
     }
 }
